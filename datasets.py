65599c3fee566487460a35454e9a739601c2a5a3
Steve McCauley <steven_mccauley@brown.edu>
2016-01-26 15:57:21 -0500

    Clean up datasets set-like interface

--- datasets.py
+++ datasets.py
@@ -1,14 +1,23 @@
-from collections import MutableSet, namedtuple
+from collections import Set, MutableSet, Iterable, namedtuple
 
 class DataSet(MutableSet):
+
+	# Defining an iterable input is essential for certain functions
+	# https://github.com/python-git/python/blob/master/Lib/_abcoll.py
+	# 
+	# Considered allowing singleton init values, but decided against.
+	# This preserves standard interface for Python sets
+	# ie: set('cat') => {'c','a','t'}
+	# ie: set(3) => TypeError: 'int' object is not iterable
 	def __init__(self, iterable=None):
 		self.data = set()
 		if iterable is not None:
-			self |= iterable
+			self |= iterable				
 
+	# BEGIN REQUIRED MutableSet DEFINITIONS
 	def __contains__(self, key):
 		if isinstance(key, Datum):
-			for d in self.data:
+			for d in self:
 				if d == key:
 					return True
 			return False
@@ -28,37 +37,49 @@ class DataSet(MutableSet):
 		return len(self.data)
 
 	def add(self, key):
-		self.data.add(key)
+		if isinstance(key, Datum):
+			self.data.add(key)
+		else:
+			raise TypeError("Expecting Datum object")
 
 	def discard(self, key):
-		self.data.discard(key)
+		if isinstance(key, Datum):
+			self.data.discard(key)
+		else:
+			raise TypeError("Expecting Datum object")
+
+	# END REQUIRED MutableSet DEFINITIONS
+
+	# BEGIN SPECIAL METHOD OVERRIDES
+	def __sub__(self, other):
+		if not isinstance(other, Iterable):
+			return NotImplemented
+		other = self._from_iterable(other)
+		return self._from_iterable(value for value in self
+									if value not in other)
+
+	def __isub__(self,other):
+		# Base abc __isub__ implementation uses self.discard
+		# This simply reuses the __sub__ implementation,
+		# which does type coersion (converts all iterables to DataSet)
+		# 
+		# Note that the return statement is necessary
+		self = self - other
+		return self
+
+	# END SPECIAL METHOD OVERRIDES
 
 	def find(self, pattern):
+		# Ordering matters due to abc __and__ implementation
+		# pattern & self != self & pattern
+		# Query datum must be in first position
 		if isinstance(pattern, DataSet):
-			if pattern <= self: #Warning!! ORDER MATTERS
-				return pattern & self # != self & pattern
-		elif isinstance(pattern, Datum):
-			if pattern in self:
-				ds = DataSet() #DataSet().add(pattern) returns NoneType?
-				ds.add(pattern)
-				return ds & self #call to self.find(ds) not working
+			if pattern <= self:
+				return pattern & self
 		else:
 			raise TypeError("expected data or dataset")
 		return DataSet()
-
-	def difference_update(self, pattern):
-		if isinstance(pattern, DataSet):
-			if pattern <= self: #Warning!! ORDER MATTERS
-				rmv = pattern & self
-				for r in rmv:
-					self.discard(r)
-		elif isinstance(pattern, Datum):
-			if pattern in self:
-				ds = DataSet() #DataSet().add(pattern) returns NoneType?
-				ds.add(pattern)
-				self.difference_update(ds)
-		else:
-			raise TypeError("expected data or dataset")
+		
 
 class Datum(namedtuple('Datum',['res', 'att', 'val'])):
 	def __eq__(self, other):
