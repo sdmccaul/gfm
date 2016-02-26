b7364e4b5e397fbd8833c38e49997d6e0ec90a3b
Steve McCauley <steven_mccauley@brown.edu>
2016-02-26 14:02:11 -0500

    Better datatype abstraction

--- graphdatatypes.py
+++ graphdatatypes.py
@@ -1,52 +1,94 @@
-############################################
-### BEGIN XSD Primitive datatypes ##########
-### https://www.w3.org/TR/xmlschema11-2/ ###
-############################################
 
-xsdNsTemplate = "\"{literal}\"^^<http://www.w3.org/2001/XMLSchema#{dtype}>"
-
-class XSDData(str):
+class DataType(str):
 	def __new__(cls, value):
 		obj = str.__new__(cls, value)
 		obj.rdf = cls.transform(value)
 		return obj
 
-class XSDBoolean(XSDData):
 	@classmethod
 	def transform(cls, value):
-		return xsdNsTemplate.format(
-			literal=value,dtype="boolean")
+		return cls.rdfTemplate.format(literal=value)
 
-# class XSDDecimal(XSDData):
+class URI(DataType):
+	rdfTemplate = "<{literal}>"
 
-# class XSDFloat(XSDData):
+############################################
+### BEGIN XSD Primitive datatypes ##########
+### https://www.w3.org/TR/xmlschema11-2/ ###
+############################################
+def xsdDataTemplate(dtype):
+	xsdNs = "<http://www.w3.org/2001/XMLSchema#{0}>".format(dtype)
+	return "\"{literal}\"^^" + xsdNs
 
-# class XSDDouble(XSDData):
+class XSDBoolean(DataType):
+	xsdType = "boolean"
+	rdfTemplate = xsdDataTemplate(xsdType)
 
-# class XSDDuration(XSDData):
+class XSDDecimal(DataType):
+	xsdType = "decimal"
+	rdfTemplate = xsdDataTemplate(xsdType)
 
-# class XSDDateTime(XSDData):
-
-# class XSDTime(XSDData):
-
-# class XSDDate(XSDData):
-
-# class XSDYearMonth(XSDData):
-
-# class XSDYear(XSDData):
-
-# class XSDMonthDay(XSDData):
-
-# class XSDDay(XSDData):
-
-# class XSDMonth(XSDData):
-
-# class XSDHexBinary(XSDData):
-
-# class XSDBase64Binary(XSDData):
-
-# class XSDAnyURI(XSDData):
-
-# class XSDQName(XSDData):
-
-# class XSDNOTATION(XSDData):
+class XSDFloat(DataType):
+	xsdType = "float"
+	rdfTemplate = xsdDataTemplate(xsdType)
+	
+class XSDDouble(DataType):
+	xsdType = "double"
+	rdfTemplate = xsdDataTemplate(xsdType)
+	
+class XSDDuration(DataType):
+	xsdType = "duration"
+	rdfTemplate = xsdDataTemplate(xsdType)
+	
+class XSDDateTime(DataType):
+	xsdType = "dateTime"
+	rdfTemplate = xsdDataTemplate(xsdType)
+	
+class XSDTime(DataType):
+	xsdType = "time"
+	rdfTemplate = xsdDataTemplate(xsdType)
+	
+class XSDDate(DataType):
+	xsdType = "date"
+	rdfTemplate = xsdDataTemplate(xsdType)
+	
+class XSDYearMonth(DataType):
+	xsdType = "gYearMonth"
+	rdfTemplate = xsdDataTemplate(xsdType)
+	
+class XSDYear(DataType):
+	xsdType = "gYear"
+	rdfTemplate = xsdDataTemplate(xsdType)
+	
+class XSDMonthDay(DataType):
+	xsdType = "gMonthDay"
+	rdfTemplate = xsdDataTemplate(xsdType)
+	
+class XSDDay(DataType):
+	xsdType = "gDay"
+	rdfTemplate = xsdDataTemplate(xsdType)
+	
+class XSDMonth(DataType):
+	xsdType = "gMonth"
+	rdfTemplate = xsdDataTemplate(xsdType)
+	
+class XSDHexBinary(DataType):
+	xsdType = "hexBinary"
+	rdfTemplate = xsdDataTemplate(xsdType)
+	
+class XSDBase64Binary(DataType):
+	xsdType = "base64Binary"
+	rdfTemplate = xsdDataTemplate(xsdType)
+	
+class XSDAnyURI(DataType):
+	xsdType = "anyURI"
+	rdfTemplate = xsdDataTemplate(xsdType)
+	
+class XSDQName(DataType):
+	xsdType = "QName"
+	rdfTemplate = xsdDataTemplate(xsdType)
+	
+class XSDNOTATION(DataType):
+	xsdType = "NOTATION"
+	rdfTemplate = xsdDataTemplate(xsdType)
+	
