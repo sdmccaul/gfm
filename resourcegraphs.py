515fc92eae6fe09f1fe609d3978a5c4c443290d1
Steve McCauley <steven_mccauley@brown.edu>
2016-03-09 10:33:21 -0500

    Reorganizing resourcegraphs

--- resourcegraphs.py
+++ resourcegraphs.py
@@ -6,14 +6,6 @@ def graphFilter(graph, key, value):
 							if getattr(d,key) == value])
 
 class ResourceGraph(MutableSet):
-
-	# Defining an iterable input is essential for certain functions
-	# https://github.com/python-git/python/blob/master/Lib/_abcoll.py
-	# 
-	# Considered allowing singleton init values, but decided against.
-	# This preserves standard interface for Python sets
-	# ie: set('cat') => {'c','a','t'}
-	# ie: set(3) => TypeError: 'int' object is not iterable
 	def __init__(self, iterable=None):
 		self.data = set()
 		self._node = None
@@ -22,19 +14,7 @@ class ResourceGraph(MutableSet):
 				next(iter(iterable)).res)
 			self |= iterable				
 
-	# BEGIN REQUIRED MutableSet DEFINITIONS
-	def __contains__(self, key):
-		for d in self:
-			if d == key:
-				return True
-		return False
-
-	def __iter__(self):
-		return self.data.__iter__()
-
-	def __len__(self):
-		return len(self.data)
-
+	#Getters and setters for Resource Graph properties
 	@property
 	def node(self):
 		return self._node
@@ -51,14 +31,18 @@ class ResourceGraph(MutableSet):
 	def successors(self):
 		return { d.val for d in self.data if isinstance(d, URI)}
 
-	def sample(self):
-		"""Copied from pop(), but no discard."""
-		it = iter(self)
-		try:
-			value = next(it)
-		except StopIteration:
-			raise KeyError
-		return value
+	# BEGIN REQUIRED MutableSet DEFINITIONS
+	def __contains__(self, key):
+		for d in self:
+			if d == key:
+				return True
+		return False
+
+	def __iter__(self):
+		return self.data.__iter__()
+
+	def __len__(self):
+		return len(self.data)
 
 	def add(self, gdata):
 		if gdata.res == self.node:
@@ -73,6 +57,15 @@ class ResourceGraph(MutableSet):
 			raise TypeError("Expecting Datum object")
 
 	# END REQUIRED MutableSet DEFINITIONS
+
+	def sample(self):
+		"""Copied from pop(), but no discard."""
+		it = iter(self)
+		try:
+			value = next(it)
+		except StopIteration:
+			raise KeyError
+		return value
 
 	def query(self, **kwargs):
 		reply = self.data
